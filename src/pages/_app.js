d78e386d393e815f21c1dd4e7c93f57c69e488d4
LokeshDeveloper4 <developer04@we-matter.com>
2024-06-23 17:03:21 +0530

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { useEffect } from "react";
+import Layout from "./components/Layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
